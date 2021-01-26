de5c1590e8301fb13663b1a21acb481d0ce77559
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-01-25 22:54:14 -0800

    adding middleware

--- app.js
+++ app.js
@@ -6,12 +6,31 @@ const express = require('express');
 // app setup 
 const app = express();
 
-// middleware
+// middleware ~ plugins or libraries we use to extend a web framework
+// used to allow Express (our web framework) to render HTML templates and send them back to the client 
+// using a new function ~ res.render('template-name', { variables }
+const exphbs = require('express-handlebars');
 
-// routes 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//routes - endpoint- get route ~ getting info to read 
+// app() is an instance of Express
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // set the url of the gif
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
 })
+
+// new route for greetings 
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name })
+})
+
 
 // start server 
 app.listen(3000, () => {
